dfdc0cf9b27391ce25c55188b3f601ec48b34500
Shamil Khannanov <shamil.khannanov@akkodis.com>
2025-01-06 08:54:52 +0100

    Added web configuration to function with React frontend

---
+++ src/main/java/com/vrubayka/restapi_sample/util/WebConfig.java
@@ -0,0 +1,16 @@
+package com.vrubayka.restapi_sample.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("http://localhost:5173") // React app origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
